5da024257d377dc1c1b9ef6042616f833aaaf238
Peter Eldritch <peter.jirak@gmail.com>
2023-06-09 19:12:00 -0700

    We got our first test to pass by just adding return false to our validatePassword function, pretty boring. We then added a second test - a password has to have eight characters, at least one number and at least one letter. We then filled in the functionality to get that toi pass, checking the length and that the password has a number and a letter.

--- validatePassword.js
+++ validatePassword.js
@@ -1,5 +1,14 @@
-function validatePassword() {
-
+function validatePassword(password) {
+    if (password === undefined || password === null) {
+        return false;
+    }
+    password = `${password}`;
+    if (password.length >= 8) {
+        if (password.match(/[a-zA-Z0-9]/)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = validatePassword;
--- validatePassword.test.js
+++ validatePassword.test.js
@@ -5,3 +5,9 @@ test("return false given an empty string",
         expect(validatePassword("")).toBe(false);
      }
 );
+
+test("return true given a password of 8 characters or longer, a letter, and a number",
+     () => {
+        expect(validatePassword('1234567a')).toBe(true);
+     }
+);
